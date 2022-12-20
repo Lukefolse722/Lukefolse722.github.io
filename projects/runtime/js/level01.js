ba636ded5c5353b7dbffe81814cd8e9e3a2f0582
Lukefolse722 <Folse554@stcharles.k12.la.us>
2022-12-20 15:31:52 +0000

    runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -27,12 +27,21 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
-
+        function createSawBlade(x,y){
+            var hitZoneSize = 25;
+        var damageFromObstacle = 10;
+        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+        sawBladeHitZone.x = 400;
+        sawBladeHitZone.y = 500;
+        game.addGameItem(sawBladeHitZone);
+        var obstacleImage = draw.bitmap("img/sawblade.png");
+        sawBladeHitZone.addChild(obstacleImage);
         
-        
-        
+    
+       
         // DO NOT EDIT CODE BELOW HERE
     }
+    
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
